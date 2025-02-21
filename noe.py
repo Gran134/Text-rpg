2e8f3e0bb986d34cd71b45d4e2db87db391807e1
Gran235 <sindresbrekke@gmail.com>
2025-02-21 13:33:52 +0100

    Update noe.py
    MEET

--- noe.py
+++ noe.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 class Player:
   def __init__(self, health, hunger, hydration, gold, damag):
@@ -14,9 +15,20 @@ class Enemy:
     self.gold = gold
     self.dameg = damag
 
-
+class Food:
+  def __init__(self, food_name, regenerate_food):
+    self.food_name = food_name
+    self.regenerate_food = regenerate_food
+    
+  
 
 player = Player(10, 10, 10, 0, 3)
 enemy = Enemy(10, 5, 3)
+meet = Food("meet", 3)
 
-print("Player: ","Health: ", player.health, "Hunger: ", player.hunger, "Hydration: ", player.hydration, "Gold: ", player.gold)
+player_choise = str(input("1. Eat: \nwhat do you want to do: "))
+
+# print("Player: ","Health: ", player.health, "Hunger: ", player.hunger, "Hydration: ", player.hydration, "Gold: ", player.gold)
+
+# os.system("cls") clear the terminal
+
