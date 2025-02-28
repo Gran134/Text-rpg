bf71c9406c1009f399773c5247edc98b1bd0901c
Gran235 <sindresbrekke@gmail.com>
2025-02-28 13:29:20 +0100

    Update noe.py
    vann og food funke

--- noe.py
+++ noe.py
@@ -2,9 +2,8 @@ import random
 import os
 
 class Player:
-  def __init__(self, health, hunger, hydration, gold, damag):
+  def __init__(self, health, hydration, gold, damag):
     self.health = health
-    self.hunger = hunger
     self.hydration = hydration
     self.gold = gold
     self.dameg = damag
@@ -19,16 +18,35 @@ class Food:
   def __init__(self, food_name, regenerate_food):
     self.food_name = food_name
     self.regenerate_food = regenerate_food
+
+class Drinks:
+  def __init__(self, drink_name, regenerate_hydration):
+    self.drink_name = drink_name
+    self.regenerate_hydration = regenerate_hydration
     
   
 
-player = Player(10, 10, 10, 0, 3)
+player = Player(10, 10, 0, 3)
 enemy = Enemy(10, 5, 3)
-meet = Food("meet", 3)
+meet = Food("meet", 5)
+water = Drinks("water", 3)
 
-player_choise = str(input("1. Eat: \nwhat do you want to do: "))
+player_choise = int(input("1. Eat: \n2. Drink: \n3. \nwhat do you want to do: "))
+
+if player_choise == 1:
+  player.health += meet.regenerate_food
+
+if player.health > 10:
+  player.health = 10
+
+if player_choise == 2:
+  player.hydration += water.regenerate_hydration
+
+if player.hydration > 10:
+  player.hydration == 10
+
+print("Health: ", player.health, "Hydration: ", player.hydration)
 
 # print("Player: ","Health: ", player.health, "Hunger: ", player.hunger, "Hydration: ", player.hydration, "Gold: ", player.gold)
 
-# os.system("cls") clear the terminal
-
+# os.system("cls") # clear the terminal
