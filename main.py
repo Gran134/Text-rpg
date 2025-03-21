0ef558755f99be2ec629ab86e9ee602031ec8ab7
Gran235 <sindresbrekke@gmail.com>
2025-03-21 10:27:49 +0000

    stats

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random 
 import os
+import time
 from player import Player
 from enemy import Enemy
 from food import Food
@@ -7,17 +8,16 @@ from drinks import Drinks
 
 
 
-os.system("cls") # clears the terminkllaaaa
 player = Player(10, 10, 0, 3) # Health, Hydration, Gold, Damage
 enemy = Enemy(10, 5, 3) # Health, Gold, Damage
 meet = Food("meet", 5)
 water = Drinks("water", 3)
+
 while True:
-  player_choise = int(input("1. Eat: \n2. Drink: \n3. Fight: \n4.  \nwhat do you want to do: "))
+  player_choise = int(input("\n1. Eat: \n2. Drink: \n3. Fight: \n4. Show stats: \n5.  \nwhat do you want to do: "))
 
   if player_choise == 1:
     player.health += meet.regenerate_food
-    continue
 
   if player_choise == 2:
     player.hydration += water.regenerate_hydration
@@ -31,10 +31,16 @@ while True:
     elif random_hit == 2:
       print(f"The enemy attaked you. You took {enemy.damege} damage.")
       player.health -= enemy.damege
+    print(f"\nPlayer: Health:  {player.health}. Hydration:  {player.hydration}")
+    print(f"Enemy: Health: {enemy.health} Gold: {enemy.gold}\n")
 
+  if player_choise == 4:
+   print(f"\nPlayer: Health: {player.health}. Hydration: {player.hydration}. Gold: {player.gold}.") 
 
-  print(f"Player: Health:  {player.health}. Hydration:  {player.hydration}")
-  print(f"Enemy: Health: {enemy.health} Gold: {enemy.gold}")
+  
+
+  time.sleep(3)
+  os.system("clear") # clears the terminkllaaa
 
   # print(f"Player: Health: {player.health} Hunger: {player.hunger} Hydration: {player.hydration} Gold: {player.gold}")
 
